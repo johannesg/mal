23df0a80b8284f64eb919c969d6413af53a2c4f0
Johannes Gustafsson <johannesgu@gmail.com>
2019-01-02 12:25:24 +0100

    refactoring

--- elixir-jg/lib/mal/reader.ex
+++ elixir-jg/lib/mal/reader.ex
@@ -1,12 +1,14 @@
 defmodule Mal.Reader do
   require Logger
 
+  alias Mal.Tokenizer
+
   def read(str) do
-    tokens = read_tokens(str)
+    tokens = Tokenizer.read_tokens(str)
 
     case read_form(tokens) do
       :eof -> :eof
-      {:error, err, _rest} -> {:error, err}
+      {:error, _} = err -> err
       {form, _rest} -> form
     end
   end
@@ -21,12 +23,12 @@ defmodule Mal.Reader do
   def read_form([{:symbol, "~"} | rest]), do: read_quote(:unquote, rest)
   def read_form([{:symbol, "@"} | rest]), do: read_quote(:deref, rest)
   def read_form([{:symbol, "^"} | rest]), do: read_meta(rest)
-  def read_form([{:error, err} | rest]), do: {:error, err, rest}
-  def read_form([{_type, _token} = form | rest]), do: {form, rest}
+  def read_form([{:error, err} | _rest]), do: {:error, err}
+  def read_form([form | rest]), do: {form, rest}
 
-  def read_list(_list, _t, []), do: {:error, :unbalanced, []}
+  def read_list(_list, _t, []), do: {:error, :unbalanced}
 
-  def read_list(_list, _t, [{:error, err} | rest]), do: {:error, err, rest}
+  def read_list(_list, _t, [{:error, err} | _rest]), do: {:error, err}
   def read_list(list, ")", [{:symbol, ")"} | rest]), do: {{:list, list}, rest}
   def read_list(list, "]", [{:symbol, "]"} | rest]), do: {{:vector, list}, rest}
 
@@ -36,12 +38,12 @@ defmodule Mal.Reader do
   end
 
   def read_form_pair([], []), do: :eof
-  def read_form_pair([_first], []), do: {:error, :expected_form, []}
+  def read_form_pair([_first], []), do: {:error, :expected_form}
   def read_form_pair([_first, _second] = forms, rest), do: {forms, rest}
 
   def read_form_pair(forms, tokens) do
     case read_form(tokens) do
-      {:error, _err, _rest} = err ->
+      {:error, _err} = err ->
         err
 
       {form, rest} ->
@@ -49,7 +51,7 @@ defmodule Mal.Reader do
     end
   end
 
-  def read_map(_map, []), do: {:error, :unclosed_map, []}
+  def read_map(_map, []), do: {:error, :unclosed_map}
   def read_map(map, [{:symbol, "}"} | rest]), do: {{:map, map}, rest}
 
   def read_map(map, tokens) do
@@ -57,7 +59,7 @@ defmodule Mal.Reader do
       :eof ->
         {:error, :unclosed_map, tokens}
 
-      {:error, _err, _rest} = err ->
+      {:error, _err} = err ->
         err
 
       {[key, value], rest} ->
@@ -73,103 +75,14 @@ defmodule Mal.Reader do
 
   def read_meta(rest) do
     case read_form_pair([], rest) do
-      {:error, _err, _rest} = err ->
+      {:error, _err} = err ->
         err
 
       :eof ->
-        {:error, :incomplete_meta, rest}
+        {:error, :incomplete_meta}
 
       {[meta, form], rest} ->
         {{:withmeta, {meta, form}}, rest}
     end
   end
-
-  defp trim("," <> rest), do: trim(rest)
-  defp trim(str), do: String.trim_leading(str)
-
-  defp read_char(str), do: String.next_codepoint(str)
-
-  def read_tokens(str) do
-    Stream.unfold(read_token(trim(str)), fn
-      :eof ->
-        nil
-
-      {:error, err, rest} ->
-        {{:error, err}, read_token(trim(rest))}
-
-      {form, rest} ->
-        {form, read_token(trim(rest))}
-    end)
-    |> Enum.to_list()
-  end
-
-  def read_token(""), do: :eof
-  #   def read_token("," <> rest), do: read_token(rest)
-  def read_token(";;" <> rest), do: ignore_comment(rest)
-  def read_token("+" <> rest), do: read_plusminus_or_number("+", rest)
-  def read_token("-" <> rest), do: read_plusminus_or_number("-", rest)
-  def read_token("\"" <> rest), do: read_string("", rest)
-  def read_token("~@" <> rest), do: {{:symbol, "~@"}, rest}
-
-  def read_token(str) do
-    {ch, rest} = read_char(trim(str))
-
-    cond do
-      String.contains?("[]{}()'`~^@", ch) -> {{:symbol, ch}, rest}
-      digit?(ch) -> read_number(str)
-      true -> read_symbol(ch, rest)
-    end
-  end
-
-  def ignore_comment(""), do: :eof
-  def ignore_comment("\n" <> rest), do: read_token(rest)
-
-  def ignore_comment(str) do
-    {_ch, rest} = read_char(str)
-    ignore_comment(rest)
-  end
-
-  def read_string(_str, "\n" <> rest), do: {:error, :newline_in_string, rest}
-  def read_string(_str, ""), do: {:error, :unclosed_string, ""}
-  def read_string(str, "\"" <> rest), do: {{:string, str}, rest}
-  def read_string(str, "\\\\" <> rest), do: read_string(str <> "\\", rest)
-  def read_string(str, "\\\"" <> rest), do: read_string(str <> "\"", rest)
-  def read_string(str, "\\n" <> rest), do: read_string(str <> "\n", rest)
-
-  def read_string(str, rest) do
-    {ch, rest} = read_char(rest)
-    read_string(str <> ch, rest)
-  end
-
-  def read_plusminus_or_number(ch, ""), do: {{:symbol, ch}, ""}
-
-  def read_plusminus_or_number(ch, rest) do
-    {ch2, _rest2} = read_char(rest)
-
-    cond do
-      digit?(ch2) -> read_number(ch <> rest)
-      true -> read_symbol(ch, rest)
-    end
-  end
-
-  def read_number(str) do
-    {number, rest} = Integer.parse(str)
-    {{:number, number}, rest}
-  end
-
-  def read_symbol(sym, ""), do: {{:symbol, sym}, ""}
-
-  def read_symbol(sym, rest) do
-    {ch, rest2} = read_char(rest)
-
-    cond do
-      String.contains?(" \n,[]{}()'\"`;", ch) -> {{:symbol, sym}, rest}
-      true -> read_symbol(sym <> ch, rest2)
-    end
-  end
-
-  def alpha?(cp), do: String.contains?("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", cp)
-  def digit?(cp), do: String.contains?("0123456789", cp)
-  def special?(cp), do: String.contains?("+-*/", cp)
-  def whitespace?(cp), do: String.contains?(" \n", cp)
 end
---
+++ elixir-jg/lib/mal/tokenizer.ex
@@ -0,0 +1,89 @@
+defmodule Mal.Tokenizer do
+
+  def read_tokens(str) do
+    Stream.unfold(read_token(trim(str)), fn
+      :eof ->
+        nil
+
+      {:error, err, rest} ->
+        {{:error, err}, read_token(trim(rest))}
+
+      {form, rest} ->
+        {form, read_token(trim(rest))}
+    end)
+    |> Enum.to_list()
+  end
+
+  defp trim("," <> rest), do: trim(rest)
+  defp trim(str), do: String.trim_leading(str)
+
+  defp read_char(str), do: String.next_codepoint(str)
+
+  def read_token(""), do: :eof
+  #   def read_token("," <> rest), do: read_token(rest)
+  def read_token(";;" <> rest), do: ignore_comment(rest)
+  def read_token("+" <> rest), do: read_plusminus_or_number("+", rest)
+  def read_token("-" <> rest), do: read_plusminus_or_number("-", rest)
+  def read_token("\"" <> rest), do: read_string("", rest)
+  def read_token("~@" <> rest), do: {{:symbol, "~@"}, rest}
+
+  def read_token(str) do
+    {ch, rest} = read_char(trim(str))
+
+    cond do
+      String.contains?("[]{}()'`~^@", ch) -> {{:symbol, ch}, rest}
+      digit?(ch) -> read_number(str)
+      true -> read_symbol(ch, rest)
+    end
+  end
+
+  def ignore_comment(""), do: :eof
+  def ignore_comment("\n" <> rest), do: read_token(rest)
+
+  def ignore_comment(str) do
+    {_ch, rest} = read_char(str)
+    ignore_comment(rest)
+  end
+
+  def read_string(_str, "\n" <> rest), do: {:error, :newline_in_string, rest}
+  def read_string(_str, ""), do: {:error, :unclosed_string, ""}
+  def read_string(str, "\"" <> rest), do: {{:string, str}, rest}
+  def read_string(str, "\\\\" <> rest), do: read_string(str <> "\\", rest)
+  def read_string(str, "\\\"" <> rest), do: read_string(str <> "\"", rest)
+  def read_string(str, "\\n" <> rest), do: read_string(str <> "\n", rest)
+
+  def read_string(str, rest) do
+    {ch, rest} = read_char(rest)
+    read_string(str <> ch, rest)
+  end
+
+  def read_plusminus_or_number(ch, ""), do: {{:symbol, ch}, ""}
+
+  def read_plusminus_or_number(ch, rest) do
+    {ch2, _rest2} = read_char(rest)
+
+    cond do
+      digit?(ch2) -> read_number(ch <> rest)
+      true -> read_symbol(ch, rest)
+    end
+  end
+
+  def read_number(str) do
+    {number, rest} = Integer.parse(str)
+    {{:number, number}, rest}
+  end
+
+  def read_symbol(sym, ""), do: {{:symbol, sym}, ""}
+
+  def read_symbol(sym, rest) do
+    {ch, rest2} = read_char(rest)
+
+    cond do
+      String.contains?(" \n,[]{}()'\"`;", ch) -> {{:symbol, sym}, rest}
+      true -> read_symbol(sym <> ch, rest2)
+    end
+  end
+
+  # def alpha?(cp), do: String.contains?("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", cp)
+  def digit?(cp), do: String.contains?("0123456789", cp)
+end
